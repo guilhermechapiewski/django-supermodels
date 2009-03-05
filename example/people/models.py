91c59da9338a2ba9ae9313c67d20fd70b836f5be
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-03-05 19:01:14 -0300

    First proof of concept. Not that compatible with Django yet.

--- example/people/models.py
+++ example/people/models.py
@@ -1,5 +1,15 @@
+'''
+import os, sys
+aqui = os.path.abspath(os.path.split(__file__)[0])
+acima = os.path.split(aqui)[0]
+sys.path.append(acima)
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+'''
+
 from django.db import models
-from django_supermodels.models import Model as SuperModel
+from django_supermodels.models import SuperModel
 
 class Person(SuperModel):
     name = models.CharField(max_length=200)
@@ -9,3 +19,9 @@ class Person(SuperModel):
     
     def __unicode__(self):
         return self.name
+
+'''    
+if __name__ == "__main__":
+    print Person.find_by_id(1)
+    print Person.find_by_name("gc")
+'''
--- src/django_supermodels/models.py
+++ src/django_supermodels/models.py
@@ -1,9 +1,15 @@
+from django.db.models.base import ModelBase as DjangoModelBase
 from django.db.models import Model as DjangoModel
 
-class Model(DjangoModel):
-    def __getattr__(self, name):
-        if name.startswith("find_by"):
+class DynamicFinders(DjangoModelBase):
+    def __getattribute__(cls, name):
+        if name.startswith("find_by_"):
             def find_by(*args, **kwargs):
-                field = name.replace("find_by_", "")
-                self.objects.get(field=kwargs[field])
-        return find_by
+                print "Missing method %s called (args = %s) and (kwargs = %s)" % (name, str(args), str(kwargs))
+            return find_by
+
+class SuperModel(DjangoModel):
+    __metaclass__ = DynamicFinders
+
+    class Meta:
+            abstract = True
